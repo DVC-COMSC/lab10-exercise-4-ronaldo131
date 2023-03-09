a65210f66021f49cf49a403ea65c760fa67c049f
RonaldoStayfresh 925 <>
2023-03-09 21:52:28 +0000

    nothing

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def main():
+celcius = int(input('Enter the Celcius Degree: '))
+fahrenheit = (9.0 / 5.0) * celcius + 32
+print(f'The Fahrenheit is {fahrenheit:.2f}')
